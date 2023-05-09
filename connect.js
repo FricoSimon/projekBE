1a15c12ce544052abdc23f1571cda49793f2f2a6
FricoSimon <simonfriko@gmail.com>
2023-05-09 16:50:32 +0700

    try connecting to GCP

--- connect.js
+++ connect.js
@@ -1,9 +1,14 @@
 const mysql = require('mysql');
 const db = mysql.createConnection({
-    host: 'localhost', // Your host
+    socketPath: '/cloudsql/latihanfrikogcp:asia-southeast2:frikodb', // your socket connection path
+    host: '34.101.119.81', // Your ip host
     user: 'root', // Your username
-    password: '', // Your password
-    database: 'universitas' // Your database name
+    password: 'friko123', // Your password
+    database: 'frikodb' // Your database name
 });
-
+if (db === 'authenticated') {
+    console.log('Database Connected!');
+} else {
+    console.log('Database Not Connected!');
+}
 module.exports = db;
