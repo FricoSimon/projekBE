717e7d03115f6f6d27e2b0d120f523ff5b6169b4
FricoSimon <simonfriko@gmail.com>
2023-05-21 14:14:11 +0700

    connect to my sql gco

--- connect.js
+++ connect.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql');
 const db = mysql.createConnection({
-    instanceName: '/cloudsql/latihanfrikogcp:asia-southeast2:frikodb', // your socket connection path
-    credentials: '',
-    host: '127.0.0.1', // Your ip host
+    instanceName: 'latihanfrikogcp:asia-southeast2:frikodb', // your socket connection path
+    credentials: 'latihanfrikogcp-cfd51cf47126.json',
+    host: '34.101.119.81', // Your ip host
     user: 'root', // Your username
     password: 'friko123', // Your password
     database: 'frikodb' // Your database name
 
 });
-if (db === 'authenticated') {
-    console.log('Database Connected!');
-} else {
-    console.log('Database Not Connected!');
-}
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Database connected.");
+});
 module.exports = db;
