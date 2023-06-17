71dca541446ea439033825c68f9cbac0b10c3bd2
FricoSimon <simonfriko@gmail.com>
2023-06-17 11:48:14 +0700

    refactoring code from callback to async/await promise

--- main.js
+++ main.js
@@ -14,115 +14,189 @@ const limiter = rateLimit({
 app.use(bodyParser.json()) // for parsing application/json
 app.use(limiter) // apply rate limiter to all requests
 
-app.get('/find', (req, res) => {
-    const page = req.query.page || 1
-    const size = req.query.size || 10
-    const offset = (page - 1) * size
+app.get('/find', async (req, res) => {
+    try {
+        const page = req.query.page || 1;
+        const size = req.query.size || 10;
+        const offset = (page - 1) * size;
 
-    const sql = `SELECT * FROM mahasiswa limit ? offset ?`
-    const values = [parseInt(size), parseInt(offset)]
-    db.query(sql, values, (err, result) => {
-        if (err) responsePost(500, null, 'Login error', res);
-        responseGet(page, size, 200, 'success', result, res)
-    })
-})
+        const sql = `SELECT * FROM mahasiswa LIMIT ? OFFSET ?`;
+        const values = [parseInt(size), parseInt(offset)];
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
+
+        responseGet(page, size, 200, 'success', result, res);
+
+    } catch (err) {
+        console.error(err.message); // Log the actual error message
+        responsePost(500, null, 'Error occurred', res);
+    }
+});
+
 
 // using params id
-app.get('/find/:id', (req, res) => {
-    const page = req.query.page || 1
-    const size = req.query.size || 10
-    const offset = (page - 1) * size
+app.get('/find/:id', async (req, res) => {
+    try {
+        const page = req.query.page || 1
+        const size = req.query.size || 10
+        const offset = (page - 1) * size
 
-    const id = req.params.id
-    const sql = `SELECT * FROM mahasiswa where id = ? limit ? offset ?`
-    const values = [parseInt(id), parseInt(size), parseInt(offset)]
-    db.query(sql, values, (err, result) => {
-        if (err) responsePost(500, null, 'Login error', res);
+        const id = req.params.id
+        const sql = `SELECT * FROM mahasiswa where id = ? limit ? offset ?`
+        const values = [parseInt(id), parseInt(size), parseInt(offset)]
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            })
+        })
+
         responseGet(page, size, 200, 'success', result, res)
-    })
+
+    } catch (err) {
+        console.error(err.message);
+        responsePost(500, null, 'Error occurred', res);
+    }
 })
 
 // using query nama
-app.get('/find/name/:name', (req, res) => {
-    const page = req.query.page || 1
-    const size = req.query.size || 10
-    const offset = (page - 1) * size
+app.get('/find/name/:name', async (req, res) => {
+    try {
+        const page = req.query.page || 1
+        const size = req.query.size || 10
+        const offset = (page - 1) * size
 
-    const name = req.params.name
-    const sql = `SELECT * FROM mahasiswa where nama = ? limit ? offset ?`;
-    const values = [name, parseInt(size), parseInt(offset)]
-    db.query(sql, values, (err, result) => {
-        if (err) responsePost(500, null, 'Login error', res);
+        const name = req.params.name
+        const sql = `SELECT * FROM mahasiswa where nama = ? limit ? offset ?`;
+        const values = [name, parseInt(size), parseInt(offset)]
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            })
+        })
+
         responseGet(page, size, 200, 'success', result, res)
-    })
+
+    } catch (err) {
+        console.error(err.message);
+        responsePost(500, null, 'Error occurred', res);
+    }
 })
 
 // test post method
-app.post('/login', (req, res) => {
-    const { nim, nama } = req.body;
-    const sql = `SELECT * FROM mahasiswa WHERE nim = ? AND nama = ?`;
-    const values = [nim, nama];
-
-    db.query(sql, values, (err, rows) => {
-        if (err)
-            responsePost(500, null, 'Login error', res);
-
-        if (rows.length === 0) {
+app.post('/login', async (req, res) => {
+    try {
+        const { nim, nama } = req.body;
+        const sql = `SELECT * FROM mahasiswa WHERE nim = ? AND nama = ?`;
+        const values = [nim, nama];
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            });
+        })
+        if (result.length === 0) {
             responsePost(404, null, 'User not found', res);
         } else {
             responsePost(200, { nim, nama }, 'Login successfully', res);
         }
-    });
+    } catch (err) {
+        console.error(err.message);
+        responsePost(500, null, 'Login error', res);
+    }
 });
 
 // input data into database (mahasiswa)
-app.post('/input', (req, res) => {
-    const { nim, nama, angkatan, jurusan } = req.body
-    const sqlCheck = `SELECT * FROM mahasiswa WHERE nim = ?`
-    const valuesCheck = [nim]
-    db.query(sqlCheck, valuesCheck, (err, result) => {
-        if (err) {
-            responsePost(500, 'input error', 'failed', res)
-            return
-        }
-        if (result?.length) {
-            responsePost(409, 'user already exist', 'failed', res)
-            return
-        }
+app.post('/input', async (req, res) => {
+    try {
+        const { nim, nama, angkatan, jurusan } = req.body;
+        const sqlCheck = `SELECT * FROM mahasiswa WHERE nim = ?`;
+        const valuesCheck = [nim];
 
-        const sql = `INSERT INTO mahasiswa (nim, nama, angkatan, jurusan) VALUES (?, ?, ?, ?)`
-        const values = [nim, nama, angkatan, jurusan] // prevent sql injection
-        db.query(sql, values, (err, result) => {
-            if (err) responsePost(500, 'input error', 'failed', res)
-            if (result?.affectedRows && result?.affectedRows > 0) responsePost(200,
-                { Id: result.insertId, nim, nama, angkatan, jurusan }, `Data received successfully`, res)
-        })
+        const existingUser = await new Promise((resolve, reject) => {
+            db.query(sqlCheck, valuesCheck, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
 
-    })
-})
+        if (existingUser.length > 0) {
+            responsePost(409, 'User already exists', 'failed', res);
+        } else {
+            const sql = `INSERT INTO mahasiswa (nim, nama, angkatan, jurusan) VALUES (?, ?, ?, ?)`;
+            const values = [nim, nama, angkatan, jurusan];
+            db.query(sql, values, (err, result) => {
+                if (err) {
+                    console.error(err.message);
+                    responsePost(500, 'Input error', 'failed', res);
+                } else if (result.affectedRows && result.affectedRows > 0) {
+                    responsePost(
+                        200,
+                        { Id: result.insertId, nim, nama, angkatan, jurusan },
+                        'Data received successfully',
+                        res
+                    );
+                }
+            });
+        }
+    } catch (err) {
+        console.error(err.message);
+        responsePost(500, 'Input error', 'failed', res);
+    }
+});
 
 // update data into database (mahasiswa)
-app.put('/update', (req, res) => {
-    const { nim, nama, angkatan, jurusan } = req.body
-    const sql = `UPDATE mahasiswa SET nama = ?, angkatan = ?, jurusan = ? WHERE nim = ?`
-    const values = [nama, angkatan, jurusan, nim]
-    db.query(sql, values, (err, result) => {
-        if (err) responsePost(500, 'update error', 'failed', res)
-        if (result?.affectedRows) responsePost(200, { changedRows: result.changedRows }, 'updated successfully', res)
-        else responsePost(404, 'user not found', 'failed', res)
-    })
+app.put('/update', async (req, res) => {
+    try {
+        const { nim, nama, angkatan, jurusan } = req.body
+        const sql = `UPDATE mahasiswa SET nama = ?, angkatan = ?, jurusan = ? WHERE nim = ?`
+        const values = [nama, angkatan, jurusan, nim]
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            })
+        })
+        if (result?.affectedRows) {
+            responsePost(200, { changedRows: result.changedRows }, 'updated successfully', res);
+        } else {
+            responsePost(404, 'user not found', 'failed', res);
+        }
+    } catch (err) {
+        console.error(err.message);
+        responsePost(500, 'update error', 'failed', res)
+    }
 })
 
 // delete data from database (mahasiswa)
-app.delete('/delete', (req, res) => {
-    const { nim } = req.body
-    const sql = `DELETE FROM mahasiswa WHERE nim = ?`
-    const values = [nim]
-    db.query(sql, values, (err, result) => {
-        if (err) responsePost(500, 'delete error', 'failed', res)
-        if (result?.affectedRows) responsePost(200, { affectedRows: result.affectedRows }, 'deleted successfully', res)
-        else responsePost(404, 'user not found', 'failed', res)
-    })
+app.delete('/delete', async (req, res) => {
+    try {
+        const { nim } = req.body
+        const sql = `DELETE FROM mahasiswa WHERE nim = ?`
+        const values = [nim]
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, values, (err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            })
+        })
+        if (result?.affectedRows) {
+            responsePost(200, { affectedRows: result.affectedRows }, 'deleted successfully', res)
+        } else {
+            responsePost(404, 'user not found', 'failed', res)
+        }
+    } catch (err) {
+        console.error(err.message);
+        responsePost(500, 'delete error', 'failed', res)
+    }
 })
 
 app.listen(port, () => {
