99f5e5c08986f700b98e3f95b9c35219dc451a08
Frico Simon <74108779+FricoSimon@users.noreply.github.com>
2023-06-17 15:46:33 +0700

    Update connect.js

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     //instanceName: 'latihanfrikogcp:asia-southeast2:frikodb', // your socket connection path
     //credentials: 'latihanfrikogcp-cfd51cf47126.json',
-    host: 'friko123.000webhostapp.com', // Your ip host
+    host: '145.14.145.115', // Your ip host
     user: 'friko', // Your username
     password: 'FrikO@123', // Your password
     database: 'id20925580_universitas' // Your database name
