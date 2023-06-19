ae0aec0c78ab25b09d498f94ec348afdf9630b76
FricoSimon <simonfriko@gmail.com>
2023-06-19 14:49:14 +0700

    add new test to find mahasiswa by id

--- testing/api.test.js
+++ testing/api.test.js
@@ -22,6 +22,24 @@ describe('GET /find?page=1&size=3', () => {
     });
 });
 
+describe('GET /find/1', () => {
+    it('Return mahasiswa by id', async () => {
+        const response = await request(app).get('/find/1');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            payload: {
+                data: [
+                    { ID: 1, NIM: 1220028, Nama: "Frico", Angkatan: 2020, Jurusan: "SI" }
+                ],
+                message: "success",
+                page: 1,
+                size: 10,
+                statuscode: 200,
+            },
+        });
+    });
+});
+
 afterAll(() => {
     db.end();
 });
